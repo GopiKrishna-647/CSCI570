09b1acf282e3670dec87d730dc56dfb82fd11278
Navya <kheterpal@usc.edu>
2021-11-24 01:25:16 -0800

    Made changes to sequenceAlignment method. Added a few functions to create and update output.txt file when needed.

--- SequenceAlignment.java
+++ SequenceAlignment.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.lang.Math;
 import java.util.*;
 
@@ -7,11 +9,16 @@ public class SequenceAlignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file = new File("/Users/gopi/Desktop/BaseTestcases_CS570FinalProject/input1.txt");
-		finalProject(file);
+
+		File input = new File(System.getProperty("user.dir") + "/input.txt");
+
+		// Create output file if it does not exist
+		createOutputFile();
+
+		finalProject(input);
 		//sequenceAlignment("AGGGCT", "AGGCA");
 	}
-	
+
 	public static void finalProject(File input){
 
 		// Array list for j and k values
@@ -56,36 +63,35 @@ public class SequenceAlignment {
 		}
 
 		// Parse first base string
-		String X = parseString(first, arrj);   
+		String X = parseString(first, arrj);
 		Integer predictedLengthX = (int)Math.pow(2, arrj.size()) * first.length();
 		if(X.length() != predictedLengthX) {
+			// TODO: Throw Error?
 			System.out.println("Not same length");
 		}
 
 		// Parse second base string
-		String Y = parseString(second, arrk);   
+		String Y = parseString(second, arrk);
 		Integer predictedLengthY = (int)Math.pow(2, arrk.size()) * second.length();
 		if(Y.length() != predictedLengthY) {
+			// TODO: Throw Error?
 			System.out.println("Not same length");
 		}
 
 		sequenceAlignment(X, Y);
-
-		System.out.println(X);
-		System.out.println(Y);
-
 	}
-	
+
 	public static String parseString(String str, ArrayList<Integer> arr){
 
 		for (int i = 0; i < arr.size(); i++){
 			String prev = str;
-			str = prev.substring(0,arr.get(i)) + prev + prev.substring(arr.get(i), prev.length());
+
+			str = prev.substring(0,arr.get(i)+1) + prev + prev.substring(arr.get(i)+1, prev.length());
 		}
 
 		return str;
 	}
-	
+
 	public static void sequenceAlignment(String X, String Y) {
 
 		// 2D arr with length of each string
@@ -105,12 +111,13 @@ public class SequenceAlignment {
 		}
 
 		// opt loop
-		for (int j = 1; j < n; j++) {
-			for (int i = 1; i < m; i++) {
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
 				if(X.charAt(i-1) == Y.charAt(j-1)) {
 					seqArr[i][j] = seqArr[i-1][j-1];
 				} else {
-					seqArr[i][j] = Math.min(seqArr[i - 1][j - 1] + misMatch(X.charAt(i-1), Y.charAt(j-1)),  Math.min(seqArr[i][j - 1] + gapPenalty, seqArr[i - 1][j] + gapPenalty));
+					seqArr[i][j] = Math.min(seqArr[i - 1][j - 1] + misMatch(X.charAt(i-1),Y.charAt(j-1)),
+													Math.min(seqArr[i][j - 1] + gapPenalty, seqArr[i - 1][j] + gapPenalty));
 				}
 			}
 		}
@@ -124,12 +131,117 @@ public class SequenceAlignment {
 			System.out.println();
 		}
 
-		// TODO: Go through the 2D Array seqArr, print output to file output.txt
+
+		String outputX = "";
+		String outputY = "";
+
+		//Traceback
+		int i = m-1;
+		int j = n-1;
+		while((i > 0) && (j > 0)){
+
+			// If X and Y characters at i-1 and j-1 are same, do nothing
+			if(X.charAt(i-1) == Y.charAt(j-1)){
+				outputX = outputX + String.valueOf(X.charAt(i-1));
+				outputY = outputY + String.valueOf(Y.charAt(j-1));
+				i--;
+				j--;
+			}
+
+			// If value of seqArr[i][j] is from vertical line gap penalty
+			else if(seqArr[i][j] == (seqArr[i][j - 1] + gapPenalty)){
+				outputX = outputX + "_";
+				outputY = outputY + String.valueOf(Y.charAt(j-1));
+				j--;
+			}
+
+			// If value of seqArr[i][j] is from horizontal line gap penalty
+			else if(seqArr[i][j] == (seqArr[i-1][j] + gapPenalty)){
+				outputX = outputX + String.valueOf(X.charAt(i-1));
+				outputY = outputY + "_";
+				i--;
+			}
+
+			// If value of seqArr[i][j] is from a mismatch and not gap penalty
+			else if(seqArr[i][j] == (seqArr[i - 1][j - 1] + misMatch(X.charAt(i-1),Y.charAt(j-1))) ){
+				outputX = outputX + String.valueOf(X.charAt(i-1));
+				outputY = outputY + String.valueOf(Y.charAt(j-1));
+				i--;
+			}
+		}
+
+		StringBuffer reverseX = new StringBuffer(outputX);
+		reverseX.reverse();
+		StringBuffer reverseY = new StringBuffer(outputY);
+		reverseY.reverse();
+
+		outputX = reverseX.toString();
+		outputY = reverseY.toString();
+
+		// For Testing Purposes
+		System.out.println(X);
+		System.out.println(Y);
+		System.out.println(outputX);
+		System.out.println(outputY);
+
+		// Parse data for the output File
+		if (outputX.length() > 50){
+			outputX = outputX.toString().substring(0,51);
+		}
+		if (outputY.length() > 50){
+			outputX = outputX.toString().substring(0,51);
+		}
+
+		addToOutputFile(outputX, outputY);
+	}
+
+	/**
+	* Create the output.txt File
+	* If it already exists, delete it and create a new one
+	*/
+	public static void createOutputFile(){
+		File output = new File(System.getProperty("user.dir"), "output.txt");
+
+		if(output.exists()){
+			output.delete();
+		}
+
+		try{
+			output.createNewFile();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	// Add data to the output.txt file
+	public static void addToOutputFile(String line1, String line2) {
+		//FileWriter output = new FileWriter(System.getProperty("user.dir") + "/output.txt", true);
+
+		try{
+			BufferedWriter buffer = new BufferedWriter(new FileWriter("output.txt"));
+
+			// Write Line1 to output file
+			if(line1!= null || !(line1 == "")){
+				buffer.write(line1);
+				buffer.newLine();
+			}
+
+			// Write line2 to output file
+			if(line2!= null || !(line2 == "")){
+				buffer.write(line2);
+				buffer.newLine();
+			}
+
+			buffer.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 
 
 	}
 
-	
 	// mismatch penalty
 	public static int misMatch(char x, char y) {
 		int mismatch = 0;
