0671f1a03cd4c5105259a89646f3d619eb6a0dd3
Navya <kheterpal@usc.edu>
2021-12-05 10:53:25 -0800

    Added comments

--- 9831987996_3051041053_5080830850.sh
+++ 9831987996_3051041053_5080830850.sh
@@ -1,8 +1,8 @@
-// Basic
+# Basic
 javac Basic_9831987996_3051041053_5080830850.java
-java Basic_9831987996_3051041053_5080830850 input.txt
+java Basic_9831987996_3051041053_5080830850 input.txt  #(and this will write to the output.txt)
 
 
-// Memory Efficient
+# Memory Efficient
 javac Efficient_9831987996_3051041053_5080830850.java
-java Efficient_9831987996_3051041053_5080830850 input.txt
+java Efficient_9831987996_3051041053_5080830850 input.txt  #(and this will write to the output.txt)
